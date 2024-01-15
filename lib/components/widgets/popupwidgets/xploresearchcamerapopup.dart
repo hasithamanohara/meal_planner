0038a4616ca952cf217a1d82fb13b8a633573da7
Hasith manohara <manoharauh.20@uom.lk>
2024-01-16 00:33:26 +0530

    image_picker_&_popup_state_manage

--- lib/components/widgets/popupwidgets/xploresearchcamerapopup.dart
+++ lib/components/widgets/popupwidgets/xploresearchcamerapopup.dart
@@ -1,5 +1,6 @@
-// bottom_popup.dart
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BottomPopupStep1 {
   static void show(BuildContext context) {
@@ -8,18 +9,21 @@ class BottomPopupStep1 {
         builder: (BuildContext context) {
           return Container(
             height: 200,
-            // decoration: BoxDecoration(
-            // color: Colors.blue, borderRadius: BorderRadius.circular(10)
-            //     ),
             child: Wrap(
               children: <Widget>[
                 ListTile(
                   title: const Text('Search Ingredent'),
-                  onTap: () => {BottomPopupStep2.show(context)},
+                  onTap: () {
+                    Navigator.pop(context);
+                    BottomPopupStep2.show(context);
+                  },
                 ),
                 ListTile(
                   title: const Text('Search Dishes'),
-                  onTap: () => {BottomPopupStep2.show(context)},
+                  onTap: () {
+                    Navigator.pop(context);
+                    BottomPopupStep2.show(context);
+                  },
                 ),
               ],
             ),
@@ -32,6 +36,7 @@ class BottomPopupStep1 {
 
 class BottomPopupStep2 {
   static void show(BuildContext context) {
+    final picker = PickImage();
     showModalBottomSheet(
         context: context,
         builder: (BuildContext context) {
@@ -44,6 +49,9 @@ class BottomPopupStep2 {
                   leading: const Icon(Icons.camera),
                   title: const Text('Take Photo'),
                   onTap: () {
+                    picker._pickImageFromGallery().then((_) {
+                      picker.show(context);
+                    });
                     // functionality for taking a photo here
                   },
                 ),
@@ -58,5 +66,32 @@ class BottomPopupStep2 {
             ),
           );
         });
+  }
+}
+
+//image picker functionality
+
+class PickImage {
+  File? _selectedImage;
+
+//pickimage from gallery
+  Future _pickImageFromGallery() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    _selectedImage = File(returnImage!.path);
+  }
+
+  void show(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+            height: 600,
+            width: double.maxFinite,
+            child: _selectedImage != null
+                ? Image.file(_selectedImage!)
+                : const Text("Please select image"));
+      },
+    );
   }
 }
